3052045c9c4933ef14d15be1497cf28f2034bb9e
Bilkent Ozgur Yazilim ve İnternet Teknolojileri Kulübü <bilkentozguryazilim@gmail.com>
2012-02-14 22:49:59 +0200

    Third and Stabil import

--- Bilkent Ulasim/src/ada/bilkent/transport/BilkentTransportActivity.java
+++ Bilkent Ulasim/src/ada/bilkent/transport/BilkentTransportActivity.java
@@ -4,17 +4,25 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.CheckBox;
 import android.widget.GridView;
+import android.widget.TextView;
 
 public class BilkentTransportActivity extends Activity {
 	/** Called when the activity is first created. */
+
+	public static final String PREFS_NAME = "Ayarlar";
+	public static boolean check = false;
 
 	// It creates the application layout when user clicked the icon of the
 	// application.
@@ -22,6 +30,44 @@ public class BilkentTransportActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+		final SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		boolean isChecked = settings.getBoolean("hatirlatma", false);
+
+		if (!check) {
+			check = true;
+			if (!isChecked) {
+				String longMessage = getString(R.string.uyari);
+				LayoutInflater inflater = getLayoutInflater();
+				final View checkboxLayout = inflater.inflate(R.layout.splash,
+						null);
+				CheckBox cb = (CheckBox) checkboxLayout
+						.findViewById(R.id.checkBox);
+				cb.setOnClickListener(new OnClickListener() {
+					public void onClick(View v) {
+						if (((CheckBox) v).isChecked()) {
+							SharedPreferences.Editor editor = settings.edit();
+							editor.putBoolean("hatirlatma", true);
+							editor.commit();
+						}
+					}
+				});
+				TextView tv = (TextView) checkboxLayout
+						.findViewById(R.id.message);
+				tv.setText(longMessage);
+				AlertDialog.Builder builder = new AlertDialog.Builder(this)
+						.setTitle("Kullaným Þartlarý")
+						.setView(checkboxLayout)
+						.setPositiveButton("Kabul Ediyorum",
+								new DialogInterface.OnClickListener() {
+									public void onClick(DialogInterface dialog,
+											int which) {
+									}
+								});
+				AlertDialog dialog = builder.create();
+				dialog.show();
+			}
+		}
 
 		GridView gridview = (GridView) findViewById(R.id.gridview);
 		gridview.setAdapter(new ImageAdapter(this));
@@ -44,6 +90,8 @@ public class BilkentTransportActivity extends Activity {
 		case R.id.info:
 			showInfo();
 			return true;
+		case R.id.exit:
+			finish();
 		default:
 			return super.onOptionsItemSelected(item);
 		}
